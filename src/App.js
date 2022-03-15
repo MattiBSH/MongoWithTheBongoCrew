7a7506b904fdb5953b56a3461d84a7ab0c31a65f
MattiBSH <cph-mh829@cphbusiness.dk>
2022-03-15 23:35:28 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 align="center">MongoDB Sharding project</h1>
+        <h2 align="center">by Matti, Ditlev and Mathias</h2>
+        <p>
+          
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
